3491086d6763ce006a2571a27026d7b65fa9ba0b
ssoldatov <s.soldatov@softpro.com>
2022-07-04 00:13:16 +0300

    mongodb connection class

---
+++ lib/database/constants.dart
@@ -0,0 +1,3 @@
+const mongoHost = 'localhost';
+const mongoPort = '27017';
+const mongoDBName = 'luck_and_roll';
---
+++ lib/database/db_connection.dart
@@ -0,0 +1,30 @@
+import 'package:luck_and_roll/database/constants.dart';
+import 'package:mongo_dart/mongo_dart.dart';
+
+class DBConnection {
+  late Db _db;
+
+  static final DBConnection _instance = DBConnection._internal();
+
+  DBConnection._internal();
+
+  static getInstance() {
+    return _instance;
+  }
+
+  static _getConnectionString() {
+    return "mongodb://$mongoHost:$mongoPort/$mongoDBName";
+  }
+
+  closeConnection() {
+    _db.close();
+  }
+
+  Future<Db> getConnection() async {
+    if (_db == null) {
+      _db = await Db.create(_getConnectionString());
+      await _db.open();
+    }
+    return _db;
+  }
+}
